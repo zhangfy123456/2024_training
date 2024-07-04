f1fc26a94ba33b42b7320a47e13d10303238c9bf
zhangfy123456 <601099270@qq.com>
2024-07-04 22:10:06 +0800

    feature:增加联系

--- pythonProject/eseb/exe2.py
+++ pythonProject/eseb/exe2.py
@@ -4,5 +4,11 @@ from exea.exe1 import Catch
 def cat_lucky_number(wd):
     print(Catch.catch(wd))
 
+def update_lucky_number(wd):
+    print(Catch.update(wd))
+
 
 cat_lucky_number(2)
+
+update_lucky_number(2)
+
--- pythonProject/exea/exe1.py
+++ pythonProject/exea/exe1.py
@@ -4,3 +4,9 @@ class Catch:
         i = id + 1
         print(f"我的幸运数字是{i}")
         return i * 2
+
+    @classmethod
+    def update(cls, id):
+        i = id + 5
+        #print(f"我的幸运数字是{i}")
+        return i * 3
